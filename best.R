09b519e2b8cbc65ad1e9faa440dae39dba8b7c71
Luis-A <luisamable@gmail.com>
2015-01-26 18:35:05 +0100

    New Files january

---
+++ best.R
@@ -0,0 +1,23 @@
+best <- function(state, outcome) {
+  
+  # reading outcome data
+  data <- read.csv("outcome-of-care-measures.csv", 
+                   colClasses ="character")
+                   #stringsAsFactors=FALSE, na.strings=c("Not available"))
+  # checking state is correct
+  if (!state %in% data[,7]) 
+     stop('Error in best("',state,'", ', '"',outcome,'") : invalid state\n')
+
+  # checking outcome is correct
+  if (outcome == "heart attack") col <- 11
+  else if (outcome =="heart failure") col <- 17
+  else if (outcome == "pneumonia") col <- 23
+  else 
+    stop('Error in best("',state,'", ', '"',outcome,'") : invalid outcome\n')
+  
+  # draw the hospital data of hospitals from param state
+  sdata <- data[data[,7]==state,]
+  
+  sort(sdata[which( as.numeric(sdata[,col]) %in% min(as.numeric(sdata[,col]), na.rm=TRUE)),2])[1]
+  
+}
